ca4e9dcf6e5099c2b2f873cf7d08f9543f3ca67c
aal_govind <aalgovind05@gmail.com>
2026-08-28 23:17:29 +0530

    created bank account application

--- CampusX_Python/OOP.py
+++ CampusX_Python/OOP.py
@@ -5,7 +5,7 @@ class Bank:
         self.balance = 0
         self.menu()
 
-    def menu(self):
+    def menu (self):
         user_input = input("""
         press 1 for set pin
         press 2 for reset pin
@@ -13,20 +13,32 @@ class Bank:
         press 4 for money withdrawl
         press 5 to exit: """)
 
+
         if user_input == '1':
             self.set_pin()
         else:
             exit()
+    
+   
 
-    def set_pin(self):
+
+    def set_pin (self):
         create_pin = input('create your first pin: ')
         self.pin = create_pin
 
         deposit = int(input('deposit your amount: '))
-        self.balance = deposit
 
-        print('your pin has been created successfully')
+        if deposit >= 5000:
+            self.balance = deposit
+        else:
+            print('minimum 5000 deposit require to open new account')
+            self.menu()
+
+        print('your pin has been created successfully: ')
         self.menu()
 
 
+
+
 obj = Bank()
+
