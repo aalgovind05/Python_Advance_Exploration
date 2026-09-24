5f4fee43b3a6b3e850865ac99d613bac62b3b9fb
aal_govind <aalgovind05@gmail.com>
2026-09-24 23:01:14 +0530

    methode over writing concept

--- CampusX_Python/04_Inheritance.py
+++ CampusX_Python/04_Inheritance.py
@@ -11,8 +11,8 @@ class User:
 
 class Student(User):         # this (User) now have connected parent class with child class so we can use that here
 
-    def __init__(self):
-        self.rollno = 100
+    def __init__(self):      # we have to erase this contructor code to access parent clase because there should be only one constructor to
+        self.rollno = 100    # so delet child constructor
 
     def inroll(self):
         print('you have inroll in new course')
